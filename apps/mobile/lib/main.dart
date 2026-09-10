c4973652a124a70525c75fe66fc2bb0cf66736af
pourya noorzadeh <46518536+PoryaNoorzadeh@users.noreply.github.com>
2026-09-10 20:01:32 +0330

    security: wire secure token storage into mobile app

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -5,14 +5,16 @@ import 'src/core/config/app_config.dart';
 import 'src/data/manisa_api.dart';
 import 'src/discovery/hub_discovery.dart';
 import 'src/realtime/manisa_realtime.dart';
+import 'src/security/token_store.dart';
 import 'src/state/app_controller.dart';
 
 void main() {
   final config = AppConfig.fromEnvironment();
   final discovery = MdnsHubDiscovery(fallback: config.hubUri);
-  final api = HttpManisaApi(discovery);
-  final realtime = ManisaRealtime(discovery);
-  final controller = AppController(api: api, realtime: realtime);
+  final tokenStore = SecureTokenStore();
+  final api = HttpManisaApi(discovery, tokenStore: tokenStore);
+  final realtime = ManisaRealtime(discovery, tokenStore: tokenStore);
+  final controller = AppController(api: api, realtime: realtime, tokenStore: tokenStore);
 
   runApp(ManisaApp(controller: controller));
 }
