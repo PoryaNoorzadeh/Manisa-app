b1b8ec2fb54fd7a7dfc4c845f982c14a54406e77
pourya noorzadeh <46518536+PoryaNoorzadeh@users.noreply.github.com>
2026-09-10 18:26:32 +0330

    feat: define replaceable Matter controller contract

---
+++ internal/matter/controller.go
@@ -0,0 +1,27 @@
+package matter
+
+import "context"
+
+type NodeID string
+
+type CommissionRequest struct {
+	SetupPayload string
+	WiFiSSID     string
+	WiFiPassword string
+}
+
+type CommissionResult struct {
+	NodeID NodeID
+}
+
+type Command struct {
+	Endpoint uint16
+	Name     string
+	Payload  map[string]any
+}
+
+type Controller interface {
+	Commission(ctx context.Context, req CommissionRequest) (CommissionResult, error)
+	RemoveNode(ctx context.Context, nodeID NodeID) error
+	Execute(ctx context.Context, nodeID NodeID, command Command) error
+}
